7e88ef574bc7e217378d18729176462718c3aeae
XVI-XVI <gooooood228@gmail.com>
2026-05-21 22:06:27 +0500

    First commit (init)

---
+++ ОЛ8ШБ9Ш.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="ОЛ8ШБ9Ш" tilewidth="32" tileheight="32" tilecount="448" columns="28">
+ <image source="C:/Users/soron/OneDrive/Desktop/Preview.png" width="918" height="515"/>
+</tileset>
